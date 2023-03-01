56a8d46cb0530c5c1eaadb0f787d5667d86a00f3
it21028014 <it21028014@my.sliit.lk>
2023-03-01 12:26:37 +0530

    success

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-console.log("hello new file");
-console.log("hey");
+
+
+const fs = require('fs');
+ fs.writeFile('file.txt', 'Hello World!', function (err) {
+       if (err) throw err;   console.log('File saved!'); 
+    }); 
